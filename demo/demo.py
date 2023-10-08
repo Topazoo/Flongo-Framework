7240ef8e89ba20291652486e6d40ba9e8dd4b14f
Peter Swanson <topazoo42@gmail.com>
2023-10-08 02:00:41 -0700

    [Responses] Add API_JSON_Response

--- demo/demo.py
+++ demo/demo.py
@@ -1,7 +1,7 @@
 from src.config.settings import AppRoutes, AppSettings
 from src.config.settings import FlaskSettings
 from src.routing import Route, RouteHandler
-from src.responses import API_Error_Response
+from src.responses import API_JSON_Response, API_Error_Response
 from src.application import Application
 
 # Method that throws a sample error
@@ -15,8 +15,10 @@ routes = AppRoutes(
         url='/error',
         handler=RouteHandler(
             # Custom handlers allow a POST request or a GET request to create different errors
-            POST=lambda request, payload: throw(f'POST - Error from payload {payload}'),
-            GET=lambda request, payload: "HELLO",
+            DELETE=lambda request, payload: throw(f'Sample error with payload {payload}'),
+            GET=lambda request, payload: "Sample GET request",
+            POST=lambda request, payload: API_JSON_Response({'sample_record': '0', 'created': True}, 201),
+            PUT=lambda request, payload: API_JSON_Response(9.99)
         )
     ),
 )
