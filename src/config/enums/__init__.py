fa346824e2cdb06e9142b72bef0395eebb52434c
Peter Swanson <topazoo42@gmail.com>
2023-10-09 22:29:51 -0700

    [Logger] Add colored logging

--- src/config/enums/__init__.py
+++ src/config/enums/__init__.py
@@ -1,5 +1,6 @@
 from .log_levels import LOG_LEVELS
 from .log_groups import LOG_GROUPS
+from .log_colors import LOG_FOREGROUND_COLORS
 from .environments import ENVIRONMENTS
 from .http_methods import HTTP_METHODS
 from .mongodb_index_types import MONGODB_INDEX_TYPES
