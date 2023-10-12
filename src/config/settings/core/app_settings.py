5bb2ca1a61f227cdd1ce9d13c3ebd57ab75b3c11
Peter Swanson <topazoo42@gmail.com>
2023-10-11 22:21:59 -0700

    [Settings] Allow settings to be stored and retrieved automatically from Flask context

--- src/config/settings/core/app_settings.py
+++ src/config/settings/core/app_settings.py
@@ -1,6 +1,8 @@
 
 from typing import Optional
 
+from flask import current_app, has_app_context
+
 from src.config.settings.app_settings.flask_settings import FlaskSettings
 from src.config.settings.app_settings.mongodb_settings import MongoDB_Settings
 
@@ -10,9 +12,25 @@ class AppSettings:
 
     def __init__(self,
             flask:Optional[FlaskSettings]=None,
-            mongo_db:Optional[MongoDB_Settings]=None
+            mongodb:Optional[MongoDB_Settings]=None
         ) -> None:
+
+        # Attempt to get the settings from a currently
+        # running Flask app if instantiated from within
+        # the app
+        flask_settings = self.get_settings_from_flask()
+        if flask_settings:
+            flask = flask or flask_settings.flask
+            mongodb = mongodb or flask_settings.mongodb
         
         # Register all created settings or create them
         self.flask = flask or FlaskSettings()
-        self.mongodb = mongo_db or MongoDB_Settings()
+        self.mongodb = mongodb or MongoDB_Settings()
+
+
+    @classmethod
+    def get_settings_from_flask(cls) -> Optional["AppSettings"]:
+        ''' Get the App settings for the current Flask app '''
+
+        if has_app_context():
+            return current_app.config.get('APP_SETTINGS')
--- src/database/mongodb/database.py
+++ src/database/mongodb/database.py
@@ -3,12 +3,14 @@ import logging
 from typing import Optional
 
 from bson import ObjectId
+from flask import current_app, has_app_context
 from src.config.enums.logs.log_levels import LOG_LEVELS
 from src.config.settings.app_settings.mongodb_settings import MongoDB_Settings
 from pymongo import TEXT, MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
 from pymongo.errors import OperationFailure
+from src.config.settings.core.app_settings import AppSettings
 
 from src.database.errors.database_error import DatabaseError
 from src.database.mongodb.fixtures import Fixtures
@@ -40,7 +42,6 @@ class MongoDB_Database:
         # TODO - Fixtures docs
     '''
 
-    # TODO - Factory from Flask context using stored settings?
     def __init__(self, 
             collection_name:Optional[str]=None, 
             database_name:Optional[str]=None, 
@@ -49,7 +50,7 @@ class MongoDB_Database:
             fixtures:Optional[Fixtures]=None,
             connection_must_be_valid:bool=True
         ):
-        self.settings = settings or MongoDB_Settings()
+        self.settings = settings or self.get_settings_from_flask() or MongoDB_Settings()
         self.indices = indices or MongoDB_Indices([])
         self.fixtures = fixtures or Fixtures({})
         self.database_name = database_name or self.settings.default_database
@@ -213,7 +214,8 @@ class MongoDB_Database:
             ))
         
 
-    def _log_and_throw_database_error(self, error:DatabaseError):
+    @classmethod
+    def _log_and_throw_database_error(cls, error:DatabaseError):
         DatabaseLogger.error(error.message)
         error.set_stack_strace(traceback.format_exc())
         if error.stack_trace:
@@ -272,3 +274,12 @@ class MongoDB_Database:
                     "fixture": fixture,
                 }
             ))
+
+
+    @classmethod
+    def get_settings_from_flask(cls) -> Optional[MongoDB_Settings]:
+        ''' Get the MongoDB settings for the current Flask app '''
+
+        app_settings = AppSettings.get_settings_from_flask()
+        if app_settings:
+            return app_settings.mongodb
