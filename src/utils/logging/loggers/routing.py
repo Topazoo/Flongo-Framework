8866a27693bb0f842f8c4a8ee25747ae0460ed34
Peter Swanson <topazoo42@gmail.com>
2023-10-12 20:24:28 -0700

    [Logging] Add stateful logging

--- src/utils/logging/loggers/routing.py
+++ src/utils/logging/loggers/routing.py
@@ -1,9 +1,7 @@
-import logging
-from src.config.enums.logs.colors.log_text_colors import LOG_TEXT_COLORS
 from src.config.enums.logs.log_groups import LOG_GROUPS
-from src.utils.logging.logging_util import LoggingUtil
+from src.utils.logging import StatefulLoggingUtil
 
-class RoutingLogger(LoggingUtil):
+class RoutingLogger(StatefulLoggingUtil):
     ''' Logger class for routes and route handling '''
 
     @property
@@ -19,24 +17,3 @@ class RoutingLogger(LoggingUtil):
     def __init__(self, url:str='', method:str='') -> None:
         self.url = url
         self.method = method
-
-    def info(self, msg:str):
-        ''' Emit a info log '''
-        self._log(msg, logging.getLogger(self.LOGGER_NAME).info, LOG_TEXT_COLORS.GREEN)
-
-    def debug(self, msg:str):
-        ''' Emit a debug log '''
-        self._log(msg, logging.getLogger(self.LOGGER_NAME).debug, LOG_TEXT_COLORS.BLUE)
-
-    def warn(self, msg:str):
-        ''' Emit a warning log '''
-        self._log(msg, logging.getLogger(self.LOGGER_NAME).warn, LOG_TEXT_COLORS.YELLOW)
-
-    def error(self, msg:str):
-        ''' Emit an error log '''
-
-        self._log(msg, logging.getLogger(self.LOGGER_NAME).error, LOG_TEXT_COLORS.RED)
-
-    def critical(self, msg:str):
-        ''' Emit a critical error log '''
-        self._log(msg, logging.getLogger(self.LOGGER_NAME).critical, LOG_TEXT_COLORS.PURPLE)
