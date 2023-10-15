db8c5acbaab27195858dcae4d6cb6fcc4eaf9443
Peter Swanson <topazoo42@gmail.com>
2023-10-15 02:31:42 -0700

    [Setup] Add setup and bumpversion config

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_requirements():
 
 setup(
     name='Flongo Framework',
-    version='0.1.0',
+    version='1.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=get_requirements(),
