b37cc5e70a5a4cf71ab37b0d71668e7a2c1f371b
Peter Swanson <topazoo42@gmail.com>
2023-10-15 02:44:09 -0700

    [Deploy] Add release workflow

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ def get_requirements():
 setup(
     name='Flongo Framework',
     version='0.1.0',
+    python_requires='>=3.9',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=get_requirements(),
