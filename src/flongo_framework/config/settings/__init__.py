bdcd4bccc0a649a9995f86ccb595aa04899db7b8
Peter Swanson <topazoo42@gmail.com>
2023-11-12 00:16:56 -0800

    [Clients] Add gmail client that loads from settings

--- src/flongo_framework/config/settings/__init__.py
+++ src/flongo_framework/config/settings/__init__.py
@@ -6,3 +6,4 @@ from .flask_settings import Flask_Settings
 from .mongodb_settings import MongoDB_Settings
 from .jwt_settings import JWT_Settings
 from .sentry_settings import Sentry_Settings
+from .gmail_settings import GMail_Settings
