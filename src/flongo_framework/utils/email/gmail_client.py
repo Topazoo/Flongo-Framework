80a6995dd76c83205e11d00d69efdbf8b4864e49
Peter Swanson <topazoo42@gmail.com>
2023-11-12 01:12:00 -0800

    [Clients] Fix gmail client

--- src/flongo_framework/utils/email/gmail_client.py
+++ src/flongo_framework/utils/email/gmail_client.py
@@ -6,8 +6,8 @@ class Gmail_Client(GmailClient):
     def __init__(self, settings:Optional[GMail_Settings]=None) -> None:
         gmail_settings = settings or App_Settings().gmail
         super().__init__(
-            gmail_settings.sender_email_address,
-            gmail_settings.refresh_token,
-            gmail_settings.client_id,
-            gmail_settings.client_secret
+            sender_email_address=gmail_settings.sender_email_address,
+            refresh_token=gmail_settings.refresh_token,
+            client_id=gmail_settings.client_id,
+            client_secret=gmail_settings.client_secret
         )
