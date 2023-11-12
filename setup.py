6890c95e8e4d8e35a35eb7c28ddc58b16327e445
Peter Swanson <topazoo42@gmail.com>
2023-11-12 01:12:06 -0800

    Bump version: 0.3.29 → 0.3.30

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_requirements():
 
 setup(
     name='flongo_framework',
-    version='0.3.29',
+    version='0.3.30',
     python_requires='>=3.9',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
